7db898ecf90400e48e8f70b9282e1591b40e5519
Geo J <you@example.com>
2018-03-12 01:49:12 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,32 +1,68 @@
 
-var cards = ["queen", "queen", "king",  "king"];
+var cards = [
+{
+	"rank": 'queen',
+	"suit": 'hearts',
+	"cardImage": "images/queen-of-hearts.png"
+},
+{
+	"rank": 'queen',
+	"suit": 'diamonds',
+	"cardImage":"images/queen-of-diamonds.png"
+
+},{
+	"rank": 'king',
+	"suit": 'hearts',
+	"cardImage": "images/king-of-hearts.png"
+},{
+	"rank": 'king',
+	"suit": 'diamonds',
+	"cardImage":"images/king-of-diamonds.png"
+}];
+
+
 var cardsInPlay=[]
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	
+	console.log("You found a match!");
+} 
+	console.log("Sorry, try again.");
+}
+
 
 function flipCard(cardId){
+
 	console.log("Up and running!");
 	var cardOne = "queen";
 	var cardTwo = "queen";
 	var cardThree = "king";
-	var cardFour = "king";
-	console.log("User flipped " + cardOne);
-	console.log("User flipped " + cardThree);
+
 	alert("you found a match")
 	var cardOne = cards[0];
-	cardsInPlay.push(cardOne);
+
+	cardsInPlay.push(cards[cardId].suit)
 	console.log("User flipped " + cardOne);
 	var cardTwo = cards[2];
-	cardsInPlay.push(cardTwo);
+	cardsInPlay.push(cards[cardId].suit)
 	console.log("User flipped " + cardTwo);
-	console.log(cardsInPlay.legnth)
-	console.log("User flipped" + cards[cardId])
+	console.log("User flipped " + cards[cardId].rank)
+
+
+	console.log("User flipped " + cards[cardId].cardImage)
+	console.log("User flipped " + cards[cardId].cardImage)
+
 	if (cardsInPlay.legnth === 2) {
 		if (cardsInPlay[0]===cardsInPlay[1]) {
 			alert("You found a match!");
-		}
-		else{
+		}else{
 			alert("Sorry, try again.");
 		}
 	}
+	checkForMatch()
 }
 
 flipCard(2)
+flipCard(3)
+
+	console.log(cardsInPlay.legnth)
